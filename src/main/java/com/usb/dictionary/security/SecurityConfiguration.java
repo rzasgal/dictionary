7f9c13b76965cfc05d626b82e392b64040a2d5fc
umut salih bayrak <bayrak.umut@gmail.com>
2022-07-23 18:10:30 +0300

    Spring security sessions disables

--- src/main/java/com/usb/dictionary/security/SecurityConfiguration.java
+++ src/main/java/com/usb/dictionary/security/SecurityConfiguration.java
@@ -3,15 +3,17 @@ package com.usb.dictionary.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/hello/**").permitAll()
-                .anyRequest().authenticated()
+        http.authorizeRequests().antMatchers("/hello/**").permitAll();
+        http.authorizeRequests().anyRequest().authenticated()
                 .and().httpBasic()
-                .and().csrf();
+                .and().csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
